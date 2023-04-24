dca3c5cefda590f0ccf0265ed8dc82df3d6aa95f
IlinykhGV <gleb@ilinyh.ru>
2023-04-24 10:35:22 +0500

    заменил бесконечный цикл на конечный до maxiter

--- optim.py
+++ optim.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def nelder_mead(f, x0, alpha=1, gamma=2, rho=0.5, sigma=0.5, tol=1e-6):
+def nelder_mead(f, x0, alpha=1, gamma=2, rho=0.5, sigma=0.5, tol=1e-6, maxiter=1000):
     '''
     f - целевая функция
     x0 - начальная точка
@@ -19,11 +19,11 @@ def nelder_mead(f, x0, alpha=1, gamma=2, rho=0.5, sigma=0.5, tol=1e-6):
         simplex.append(point)
 
     # Основной цикл
-    while True:
+    for iter in range(maxiter):
         # Шаг 1: сортировка вершин по значению функции
         simplex.sort(key=f)
 
-        # Шаг 2: вычисление центра тяжести
+        # Шаг 2: вычисление центра тяжести без худшей точки
         centroid = np.mean(simplex[:-1], axis=0)
 
         # Шаг 3: отражение
@@ -43,7 +43,7 @@ def nelder_mead(f, x0, alpha=1, gamma=2, rho=0.5, sigma=0.5, tol=1e-6):
             if f(xc) < f(simplex[-1]):
                 simplex[-1] = xc
             else:
-                # Шаг 6: уменьшение
+                # Шаг 6: уменьшение глобальное сжатие симплекса к точке с наименьшим значением
                 for i in range(1, len(simplex)):
                     simplex[i] = simplex[0] + sigma * (simplex[i] - simplex[0])
 
@@ -51,17 +51,5 @@ def nelder_mead(f, x0, alpha=1, gamma=2, rho=0.5, sigma=0.5, tol=1e-6):
         if all([np.linalg.norm(simplex[0] - simplex[i]) < tol for i in range(1, len(simplex))]):
             break
 
-    return simplex[0]
+    return simplex[0], iter
 
-
-def rosenbrock(x):
-    return (1-x[0])**2 + 100*(x[1]-x[0]**2)**2
-
-
-def ff(x):
-    return x[0]**2+(x[1]-2)**2+x[2]**2
-
-
-x0 = np.array([1.0, 1.0, 1.0])
-xmin = nelder_mead(ff, x0)
-print('Минимум:', xmin)
