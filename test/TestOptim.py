0210f72e925a63bdd1f8f14349537d8675d0c75a
IlinykhGV <Ilinykh.pnipu@yandex.ru>
2023-04-25 21:05:54 +0500

    добавил тест со штрафной функцией

--- test/TestOptim.py
+++ test/TestOptim.py
@@ -17,6 +17,11 @@ class Test_Optim:
                                np.array([0.0, 2.0, 0.0])),
                               ((lambda x: (1-x[0])**2 + 100*(x[1]-x[0]**2)**2, np.array([1.0, 1.0, 1.0])),
                                np.array([1.0, 1.0, 1.0])),
+                              ((lambda x: x[0]+x[1]+1e5*sum([max(-y, 0)**2 for y in x]), np.array([1.0, 1.0])),
+                               np.array([0.0, 0.0])),
+                              ((lambda x: x[0]*x[2]+x[1]*x[3]+1e3*sum([max(-y, 0)**2 for y in x]), #+x[1]*x[3]
+                                np.array([1.0, 1.0, 1.0, 1.0])),
+                               np.array([0.0, 0.0, 0.0, 0.0])),
                               ])
     def test_nelder_mead(self, input_data, expected_result):
         EPS = 1e-5
