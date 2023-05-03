c08eb35c94011b0c248fdb73b7300369c658bfaf
IlinykhGV <gleb@ilinyh.ru>
2023-05-03 14:08:22 +0500

    изменил выходные данные из nelder_mead и убрал лишний тест

--- test/TestOptim.py
+++ test/TestOptim.py
@@ -1,3 +1,5 @@
+"""Модуль содержит тесты модуля оптимизации"""
+
 import pytest
 import numpy as np
 from optim import nelder_mead
@@ -19,16 +21,13 @@ class Test_Optim:
                                np.array([1.0, 1.0, 1.0])),
                               ((lambda x: x[0]+x[1]+1e5*sum([max(-y, 0)**2 for y in x]), np.array([1.0, 1.0])),
                                np.array([0.0, 0.0])),
-                              ((lambda x: x[0]*x[2]+x[1]*x[3]+1e3*sum([max(-y, 0)**2 for y in x]), #+x[1]*x[3]
-                                np.array([1.0, 1.0, 1.0, 1.0])),
-                               np.array([0.0, 0.0, 0.0, 0.0])),
                               ])
     def test_nelder_mead(self, input_data, expected_result):
         EPS = 1e-5
         ff = input_data[0]
         x0 = input_data[1]
 
-        xmin, iters = nelder_mead(ff, x0)
+        (xmin, iters), time = nelder_mead(ff, x0)
         actual_result = xmin
 
         assert np.alltrue(abs(actual_result - expected_result) <= EPS)
