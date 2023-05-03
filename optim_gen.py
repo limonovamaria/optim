86dedbcceceb71908e4bcb6e02676a829109e8ec
IlinykhGV <gleb@ilinyh.ru>
2023-05-03 14:12:51 +0500

    параметризировал ограничение на единственность парамтра в группе

--- optim_gen.py
+++ optim_gen.py
@@ -1,7 +1,4 @@
-"""Модуль содержит методы формирования целевых функций (включая штрафные)"""
-
-import numpy as np
-from optim import nelder_mead
+"""Модуль формирования целевых функций (включая штрафные)"""
 
 
 def take_first(ar, n):
@@ -51,9 +48,9 @@ def create_func(k_targ, ka, kb, kc, kd, a_min, a_max, b_min, b_max, c_min, c_max
     return inner_method
 
 
-def create_func_2(k_targ, ka, kb, kc, kd, a_min, a_max, b_min, b_max, c_min, c_max, d_min, d_max, k=100, p=2):
+def create_func_2(k_targ, ka, kb, kc, kd, a_min, a_max, b_min, b_max, c_min, c_max, d_min, d_max, k=1e3, p=2):
     def inner_method(x):
-
+        EPS = 1.0  # Значение, ниже которого второй по величине параметр в группе считается подходящим
         # TODO проверку на неодинаковость размерности
         xa, x = take_first(x, len(ka))
         xb, x = take_first(x, len(kb))
@@ -72,16 +69,17 @@ def create_func_2(k_targ, ka, kb, kc, kd, a_min, a_max, b_min, b_max, c_min, c_m
         for xx in [xa, xb, xc, xd]:
             f_res += k * sum([max(-xi, 0) ** p for xi in xx])
 
-        # ограничения на мин. каждой группы
+        # Ограничения на мин. каждой группы
         for xx, mmin in zip([xa, xb, xc, xd], [a_min, b_min, c_min, d_min]):
             f_res += k * max(-(sum([xi for xi in xx]) - mmin), 0) ** p
-        # ограничения на макс. каждой группы
+        # Ограничения на макс. каждой группы
         for xx, mmax in zip([xa, xb, xc, xd], [a_max, b_max, c_max, d_max]):
             f_res += k * max(-(mmax - sum([xi for xi in xx])), 0) ** p
 
+        # ограничение на один параметр в группе
         for xx in [xa, xb, xc, xd]:
             xx_new = sorted(xx)
-            f_res += k * max(-(xx_new[-1] - (xx_new[-2] * 10**2)), 0) ** p
+            f_res += k * max(xx_new[-2]-EPS, 0) ** p
 
         return f_res
 
