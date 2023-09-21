913cbaad4b57e95f7eb6e243a1f0ee7e7a127325
Gleb Ilinyh <gleb@ilinyh.ru>
2023-09-21 23:25:53 +0500

    обновил функцию create_func
     - добавил ограничения на наличие продуктов
     - изменил названия на более понятные

--- optim_gen.py
+++ optim_gen.py
@@ -5,38 +5,52 @@ def take_first(ar, n):
     return ar[:n], ar[n:]
 
 
-def create_func(k_targ, ka, kb, kc, kd, a_min, a_max, b_min, b_max, c_min, c_max, d_min, d_max, k=1e3, p=2):
+def create_func(food_energy_goal,
+                food_energy_groups,
+                limits_groups,
+                ga, gb, gc, gd,
+                penalty=1e3, penalty_power=2):
     def inner_method(x):
         EPS = 1.0  # Значение, ниже которого второй по величине параметр в группе считается подходящим
         # TODO проверку на неодинаковость размерности
+
+        a_min, a_max, b_min, b_max, c_min, c_max, d_min, d_max = limits_groups
+
+        ka, kb, kc, kd = food_energy_groups
+
         xa, x = take_first(x, len(ka))
         xb, x = take_first(x, len(kb))
         xc, x = take_first(x, len(kc))
         xd, x = take_first(x, len(kd))
 
-        k_calc = 0
-        k_calc += sum([xi * ki for xi, ki in zip(xa, ka)])
-        k_calc += sum([xi * ki for xi, ki in zip(xb, kb)])
-        k_calc += sum([xi * ki for xi, ki in zip(xc, kc)])
-        k_calc += sum([xi * ki for xi, ki in zip(xd, kd)])
+        food_energy_calc = 0
+        food_energy_calc += sum([xi * ki for xi, ki in zip(xa, ka)])
+        food_energy_calc += sum([xi * ki for xi, ki in zip(xb, kb)])
+        food_energy_calc += sum([xi * ki for xi, ki in zip(xc, kc)])
+        food_energy_calc += sum([xi * ki for xi, ki in zip(xd, kd)])
 
-        f_res = (k_calc - k_targ)**2
+        f_res = (food_energy_calc - food_energy_goal)**2
 
         # ограничения на неотрицательность
         for xx in [xa, xb, xc, xd]:
-            f_res += k * sum([max(-xi, 0) ** p for xi in xx])
+            f_res += penalty * sum([max(-xi, 0)**penalty_power for xi in xx])
 
         # Ограничения на мин. каждой группы
         for xx, mmin in zip([xa, xb, xc, xd], [a_min, b_min, c_min, d_min]):
-            f_res += k * max(-(sum([xi for xi in xx]) - mmin), 0) ** p
+            f_res += penalty * max(-(sum([xi for xi in xx]) - mmin), 0)**penalty_power
         # Ограничения на макс. каждой группы
         for xx, mmax in zip([xa, xb, xc, xd], [a_max, b_max, c_max, d_max]):
-            f_res += k * max(-(mmax - sum([xi for xi in xx])), 0) ** p
+            f_res += penalty * max(-(mmax - sum([xi for xi in xx])), 0)**penalty_power
 
         # ограничение на один параметр в группе
         for xx in [xa, xb, xc, xd]:
             xx_new = sorted(xx)
-            f_res += k * max(xx_new[-2]-EPS, 0) ** p
+            f_res += penalty * max(xx_new[-2] - EPS, 0)**penalty_power
+
+        # ограничение на граммовки
+        for xx, gg in zip([xa, xb, xc, xd], [ga, gb, gc, gd]):
+            for xi, gi in zip(xx, gg):
+                f_res += penalty * max(xi - gi, 0)**penalty_power
 
         return f_res
 
--- optim_test.py
+++ optim_test.py
@@ -58,8 +58,21 @@ c_max = 500
 d_min = 50
 d_max = 500
 
+#граммовки продуктов
+ga = [500, 360, 400]
+gb = [500, 360, 400, 200]
+gc = [500, 360, 400, 200]
+gd = [500, 360, 400]
+
 # 1
-ff = create_func(k_targ, ka, kb, kc, kd, a_min, a_max, b_min, b_max, c_min, c_max, d_min, d_max, k=1e1, p=2)
+ff = create_func(k_targ,
+                 (ka, kb, kc, kd),
+                 (a_min, a_max,
+                  b_min, b_max,
+                  c_min, c_max,
+                  d_min, d_max),
+                 ga, gb, gc, gd,
+                 penalty=1e1, penalty_power=2)
 x0 = np.zeros((len(ka) + len(kb) + len(kc) + len(kd)))  # *2
 # x0 = np.random.random_sample(len(ka) + len(kb) + len(kc) + len(kd)) * 100
 (res, iter), time = nelder_mead(ff, x0, gamma=2, maxiter=20000, dx=100, stop=400.)
