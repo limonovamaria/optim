6b8ba1c9b4687cb7fafed353be86611b4743bc5c
Gleb Ilinyh <gleb@ilinyh.ru>
2023-09-22 01:07:02 +0500

    добавил код для дальнейшего решения задачи унификации числа групп

--- optim_gen.py
+++ optim_gen.py
@@ -2,6 +2,9 @@
 
 
 def take_first(ar, n):
+    """
+    разделяет список на два списка размерами [n] и [len(ar)-n]
+    """
     return ar[:n], ar[n:]
 
 
@@ -13,22 +16,48 @@ def create_func(food_energy_goal,
     """
     создаёт функцию, f(x), где x - список граммовок продуктов, f - целевая функция со штрафами
     """
+    zero = 0
+
+    a_min, a_max, b_min, b_max, c_min, c_max, d_min, d_max = groups_limits
+
+    ka, kb, kc, kd = food_energy_groups
+
+    ga, gb, gc, gd = food_limits
+
+    lens = tuple(map(len, food_energy_groups))
+
+    lenka = len(ka)
+    lenkb = len(kb)
+    lenkc = len(kc)
+    lenkd = len(kd)
+
+    def pairwise(arr):
+        # for i in range(0, len(arr), 2):
+        #     yield arr[i], arr[i + 1]
+        yield arr[0:lenka]
+        yield arr[lenka:lenka]
+        yield arr[0:lenka]
+        yield arr[0:lenka]
+
+    for pair in pairwise(food_energy_groups):
+        print(pair)
+
+    print(pairwise(my_array))
     def inner_method(x):
         EPS = 1.0  # Значение, ниже которого второй по величине параметр в группе считается подходящим
         # TODO проверку на неодинаковость размерности
 
-        a_min, a_max, b_min, b_max, c_min, c_max, d_min, d_max = groups_limits
+        quantity_group = []
+        # for food_energy_group in food_energy_groups:
+        #     xa, x = take_first(x, len(food_energy_group))
+            # quantity_group
 
-        ka, kb, kc, kd = food_energy_groups
+        xa, x = take_first(x, lenka)
+        xb, x = take_first(x, lenkb)
+        xc, x = take_first(x, lenkc)
+        xd, x = take_first(x, lenkd)
 
-        ga, gb, gc, gd = food_limits
-
-        xa, x = take_first(x, len(ka))
-        xb, x = take_first(x, len(kb))
-        xc, x = take_first(x, len(kc))
-        xd, x = take_first(x, len(kd))
-
-        food_energy_calc = 0
+        food_energy_calc = zero
         food_energy_calc += sum([xi * ki for xi, ki in zip(xa, ka)])
         food_energy_calc += sum([xi * ki for xi, ki in zip(xb, kb)])
         food_energy_calc += sum([xi * ki for xi, ki in zip(xc, kc)])
