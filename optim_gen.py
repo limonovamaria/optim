abce3301abd3c23c6f9216e5765922b46f071c61
IlinyhGleb <97687852+IlinyhGleb@users.noreply.github.com>
2023-09-22 21:50:44 +0500

    Multi groups (#5)
    * удалил create_function, create_function2 переименовал в create_function
    
    * обновил функцию create_func
    
     - добавил ограничения на наличие продуктов
     - изменил названия на более понятные
    
    * укрупнил входящие параметры
    
    * добавил код для дальнейшего решения задачи унификации числа групп
    
    * сделал обработку любого количества групп
    
    * обновил .gitignore
    
    * обновил исходные данные
    
    * добавил исключение по ограничению на группу из одного продукта
    
    * убрал лишний параметр
    
    * сделал вывод результатов более универсальным
    
    * обновил исходные данные

--- optim_gen.py
+++ optim_gen.py
@@ -1,85 +1,68 @@
 """Модуль формирования целевых функций (включая штрафные)"""
 
+import numpy as np
+
 
 def take_first(ar, n):
+    """
+    разделяет список на два списка размерами [n] и [len(ar)-n]
+    """
     return ar[:n], ar[n:]
 
 
-def create_func(k_targ, ka, kb, kc, kd, a_min, a_max, b_min, b_max, c_min, c_max, d_min, d_max, k=1000, p=2):
-    def inner_method(x):
-        take_first = lambda ar, n: (ar[:n], ar[n:])
+def create_func(food_energy_goal,
+                groups,
+                food_energy_groups,
+                group_limits_min,
+                group_limits_max,
+                food_limits,
+                penalty=1e3, penalty_power=2):
+    """
+    создаёт функцию, f(x), где x - список граммовок продуктов, f - целевая функция со штрафами
+    """
 
-        # TODO проверку на неодинаковость размерности
-        xa, x = take_first(x, len(ka))
-        xb, x = take_first(x, len(kb))
-        xc, x = take_first(x, len(kc))
-        xd, x = take_first(x, len(kd))
-        a, x = take_first(x, len(ka))
-        b, x = take_first(x, len(kb))
-        c, x = take_first(x, len(kc))
-        d, x = take_first(x, len(kd))
+    result = []
+    start = 0
+    for i in groups:
+        end = start + i
+        result.append((start, end))
+        start = end
 
-        k_calc = sum([xi * gi * ki for xi, gi, ki in zip(xa, a, ka)])
-        k_calc += sum([xi * gi * ki for xi, gi, ki in zip(xb, b, kb)])
-        k_calc += sum([xi * gi * ki for xi, gi, ki in zip(xc, c, kc)])
-        k_calc += sum([xi * gi * ki for xi, gi, ki in zip(xd, d, kd)])
+    def groupwise(arr):
+        for group in result:
+            yield arr[group[0]: group[1]]
 
-        f_res = (k_calc - k_targ) ** 2
-
-        for x in [xa, xb, xc, xd, a, b, c, d]:
-            f_res += k * sum([max(-y, 0) ** p for y in x])
-        for x in [a, b, c, d]:
-            f_res += k * sum([max(-y, 0) ** p for y in x])
-        for x in [a, b, c, d]:
-            f_res += k * sum([max(y-100, 0) ** p for y in x])
-
-        f_res += k * max(-(sum([xi*gi for xi, gi in zip(xa, a)])-a_min), 0) ** p
-        f_res += k * max(-(sum([xi*gi for xi, gi in zip(xb, b)])-b_min), 0) ** p
-        f_res += k * max(-(sum([xi*gi for xi, gi in zip(xc, c)])-c_min), 0) ** p
-        f_res += k * max(-(sum([xi*gi for xi, gi in zip(xd, d)])-d_min), 0) ** p
-
-        f_res += k * max(-(a_max-(sum([xi*gi for xi, gi in zip(xa, a)]))), 0) ** p
-        f_res += k * max(-(b_max-(sum([xi*gi for xi, gi in zip(xb, b)]))), 0) ** p
-        f_res += k * max(-(c_max-(sum([xi*gi for xi, gi in zip(xc, c)]))), 0) ** p
-        f_res += k * max(-(d_max-(sum([xi*gi for xi, gi in zip(xd, d)]))), 0) ** p
-
-        return f_res
-
-    return inner_method
-
-
-def create_func_2(k_targ, ka, kb, kc, kd, a_min, a_max, b_min, b_max, c_min, c_max, d_min, d_max, ga, gb, gc, gd, k=1e3, p=2):
     def inner_method(x):
         EPS = 1.0  # Значение, ниже которого второй по величине параметр в группе считается подходящим
         # TODO проверку на неодинаковость размерности
-        xa, x = take_first(x, len(ka))
-        xb, x = take_first(x, len(kb))
-        xc, x = take_first(x, len(kc))
-        xd, x = take_first(x, len(kd))
 
-        k_calc = 0
-        k_calc += sum([xi * ki for xi, ki in zip(xa, ka)])
-        k_calc += sum([xi * ki for xi, ki in zip(xb, kb)])
-        k_calc += sum([xi * ki for xi, ki in zip(xc, kc)])
-        k_calc += sum([xi * ki for xi, ki in zip(xd, kd)])
+        food_energy_calc = 0
+        food_energy_calc += sum(x * food_energy_groups)
 
-        f_res = (k_calc - k_targ) ** 2
+        f_res = (food_energy_calc - food_energy_goal)**2
 
         # ограничения на неотрицательность
-        for xx in [xa, xb, xc, xd]:
-            f_res += k * sum([max(-xi, 0) ** p for xi in xx])
+        for xi in x:
+            f_res += penalty * max(-xi, 0)**penalty_power
 
         # Ограничения на мин. каждой группы
-        for xx, mmin in zip([xa, xb, xc, xd], [a_min, b_min, c_min, d_min]):
-            f_res += k * max(-(sum([xi for xi in xx]) - mmin), 0) ** p
+        for xx, mmin in zip(groupwise(x), group_limits_min):
+            f_res += penalty * max(-(sum(xx) - mmin), 0)**penalty_power
+
         # Ограничения на макс. каждой группы
-        for xx, mmax in zip([xa, xb, xc, xd], [a_max, b_max, c_max, d_max]):
-            f_res += k * max(-(mmax - sum([xi for xi in xx])), 0) ** p
+        for xx, mmax in zip(groupwise(x), group_limits_max):
+            f_res += penalty * max(-(mmax - sum(xx)), 0)**penalty_power
+
+        # ограничение на граммовки
+        for op in x - food_limits:
+            f_res += penalty * max(op, 0)**penalty_power
 
         # ограничение на один параметр в группе
-        for xx in [xa, xb, xc, xd]:
+        for xx in groupwise(x):
+            if len(xx) <= 1:
+                continue
             xx_new = sorted(xx)
-            f_res += k * max(xx_new[-2]-EPS, 0) ** p
+            f_res += penalty * max(xx_new[-2] - EPS, 0)**penalty_power
 
 
         #ограничение на граммовки
