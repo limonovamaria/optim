77a5c9233843daed6d230bd52f4312302e9943e6
Лимонова Мария <112612418+limonovamaria@users.noreply.github.com>
2023-10-25 00:24:46 +0500

    Бд и процедуры
    Прописала бд, заполнила, прописала процедуры для вытаскивания нужных элементов, протестировала.

--- json_server/app.py
+++ json_server/app.py
@@ -1,6 +1,8 @@
+import numpy
 from flask import Flask, request, jsonify
 from optim import create_func, nelder_mead
 import numpy as np
+import sqlite3
 
 app = Flask(__name__)
 
@@ -11,13 +13,46 @@ def flatten(list_of_lists):
 
 @app.route('/optim', methods=['POST'])
 def get_optim_solu():
+    ##
+
+    connection = sqlite3.connect('my_database.db')
+
+    req_Json = request.json
+    cursor = connection.cursor()
+    """""
+    cursor.execute('SELECT username FROM Users where age = ?', (28,))
+    usernames = cursor.fetchall()
+    username = " "
+    # Выводим результаты
+    for user in usernames:
+        username = user
+    """""
+
+
+
     data = request.get_json()
     food_energy_goal = data['food_energy_goal']
 
     # калорийности продуктов (необходимо получить из БД с размерностью ккал/гр)
     KKAL_IN_GR = 0.01
+    #
+    ref_id = 1
+    cursor.execute(
+        'SELECT refrigerator_id, product_id, caloricity, categories_id FROM refrigerator_has_product JOIN  product WHERE product_id = id AND refrigerator_id = ?',
+        (ref_id,))
+    groups = cursor.fetchall()
+
+    food_energy_groups = []
+    for i in range(1, 9):
+        b = []
+        for x in groups:
+            if (x[3] == i):
+                b = np.append(b, x[2])
+        food_energy_groups.append(b)
+    #
+    '''
     food_energy_groups = [
-        [k * KKAL_IN_GR for k in [68]],
+        [k * KKAL_IN_GR for k in [68]],# брать из бд 1 группа 1 продукт
         [k * KKAL_IN_GR for k in [343, 360]],
         [k * KKAL_IN_GR for k in [170]],
         [k * KKAL_IN_GR for k in [52, 89, 48]],
@@ -25,21 +60,57 @@ def get_optim_solu():
         [k * KKAL_IN_GR for k in [259, 366]],
         [k * KKAL_IN_GR for k in [40, 159]],
         [k * KKAL_IN_GR for k in [15, 18]]]
+    '''
+    for k in food_energy_groups:
+        for i in range(0, len(k)):
+            k[i] = k[i]*KKAL_IN_GR
+
+
 
     # граммовки продуктов (необходимо получить из БД с размерностью гр)
-    food_quantity_groups = [
-        [200],
-        [300, 200],
+    cursor.execute(
+        'SELECT refrigerator_id, product_id, amount, categories_id FROM refrigerator_has_product JOIN  product WHERE product_id = id AND refrigerator_id = ?',
+        (ref_id,))
+    groups = cursor.fetchall()
+    food_quantity_groups = []
+    for i in range(1, 9):
+        b = []
+        for x in groups:
+            if (x[3] == i):
+                b = np.append(b, x[2])
+        food_quantity_groups.append(b)
+
+    '''
+    food_quantity_groups = [ # брать от пользователя
+        [200],# 1 группа 1 продукт
+        [300, 200],# 2 группа 2 продукта
         [500],
         [200, 200, 150],
         [40, 50],
         [300, 150],
         [1000, 500],
         [200, 200]]
+    '''
 
+    cursor.execute('SELECT min FROM limits')
+    l_min = cursor.fetchall()
+    limits_min = np.zeros(8)
+    k = 0
+    for x in l_min:
+        limits_min[k] = x[0]
+        k = k + 1
+    #print(min)
     # ограничения на холодильник (необходимо получить из БД с размерностью гр)
-    limits_min = [50, 50, 50, 50, 10, 10, 50, 50]
-    limits_max = [200, 200, 300, 200, 30, 30, 200, 150]
+    #limits_min = [50, 50, 50, 50, 10, 10, 50, 50] # брать из бд, ограничения по каждой группе
+
+    cursor.execute('SELECT max FROM limits')
+    l_max = cursor.fetchall()
+    limits_max = np.zeros(8)
+    k = 0
+    for x in l_max:
+        limits_max[k] = x[0]
+        k = k + 1
+    #limits_max = [200, 200, 300, 200, 30, 30, 200, 150]
 
     group_limits_min = np.array(limits_min)
     group_limits_max = np.array(limits_max)
@@ -59,7 +130,7 @@ def get_optim_solu():
     x0 = np.zeros(len(food_energy_groups_array))
 
     (res, iter), time = nelder_mead(ff, x0, gamma=2, maxiter=20000, dx=10)
-
+    connection.close()
     return jsonify({'result': res.tolist()})
 
 
---
+++ json_server/db.py
@@ -0,0 +1,204 @@
+import sqlite3
+import numpy
+
+connection = sqlite3.connect('my_database.db')
+cursor = connection.cursor()
+"""""
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS User (
+login TEXT PRIMARY KEY,
+password TEXT NOT NULL
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS person (
+id INT PRIMARY KEY,
+name TEXT NOT NULL,
+surname TEXT NOT NULL,
+age INT NOT NULL,
+height INT NOT NULL,
+weight INT NOT NULL,
+activity_level INT NOT NULL,
+sex INT,
+user_login TEXT,
+FOREIGN KEY (user_login) REFERENCES User
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS refrigerator (
+id INT PRIMARY KEY,
+person_id INT,
+FOREIGN KEY (person_id) REFERENCES person
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS categories (
+id INT PRIMARY KEY,
+name TEXT
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS limits (
+min INT,
+max INT,
+categories_id INT,
+FOREIGN KEY (categories_id) REFERENCES categories
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS product (
+id INT PRIMARY KEY,
+name TEXT,
+caloricity INT,
+categories_id INT,
+FOREIGN KEY (categories_id) REFERENCES categories
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS refrigerator_has_product (
+refrigerator_id INT,
+product_id INT,
+amount DOUBLE NOT NULL,
+PRIMARY KEY(refrigerator_id,product_id),
+FOREIGN KEY (product_id) REFERENCES product,
+FOREIGN KEY (refrigerator_id) REFERENCES refrigerator
+)
+''')
+
+
+cursor.execute('INSERT INTO User (login, password) VALUES (?, ?)', ('admin', '123'))
+cursor.execute('INSERT INTO person (id, name, surname, age, height, weight, activity_level, sex, user_login) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', (1, 'Мария', 'Лимонова', 21, 164, 48, 2, 0, 'admin'))
+cursor.execute('INSERT INTO refrigerator (id, person_id) VALUES (?, ?)', (1, 1))
+
+cursor.execute('INSERT INTO categories (id, name) VALUES (?, ?)', (1, 'Каши'))
+cursor.execute('INSERT INTO categories (id, name) VALUES (?, ?)', (2, 'Крупы'))
+cursor.execute('INSERT INTO categories (id, name) VALUES (?, ?)', (3, 'Мясо/рыба'))
+cursor.execute('INSERT INTO categories (id, name) VALUES (?, ?)', (4, 'Фрукты'))
+cursor.execute('INSERT INTO categories (id, name) VALUES (?, ?)', (5, 'Орехи'))
+cursor.execute('INSERT INTO categories (id, name) VALUES (?, ?)', (6, 'Мучное'))
+cursor.execute('INSERT INTO categories (id, name) VALUES (?, ?)', (7, 'Молочные продукты'))
+cursor.execute('INSERT INTO categories (id, name) VALUES (?, ?)', (8, 'Овощи'))
+
+cursor.execute('INSERT INTO limits (min, max, categories_id) VALUES (?, ?, ?)', (50, 200, 1))
+cursor.execute('INSERT INTO limits (min, max, categories_id) VALUES (?, ?, ?)', (50, 200, 2))
+cursor.execute('INSERT INTO limits (min, max, categories_id) VALUES (?, ?, ?)', (50, 300, 3))
+cursor.execute('INSERT INTO limits (min, max, categories_id) VALUES (?, ?, ?)', (50, 200, 4))
+cursor.execute('INSERT INTO limits (min, max, categories_id) VALUES (?, ?, ?)', (10, 30, 5))
+cursor.execute('INSERT INTO limits (min, max, categories_id) VALUES (?, ?, ?)', (10, 30, 6))
+cursor.execute('INSERT INTO limits (min, max, categories_id) VALUES (?, ?, ?)', (50, 200, 7))
+cursor.execute('INSERT INTO limits (min, max, categories_id) VALUES (?, ?, ?)', (50, 150, 8))
+
+
+cursor.execute('INSERT INTO product (id, name, caloricity, categories_id) VALUES (?, ?, ?, ?)', (1, 'Овсянка', 68, 1))
+cursor.execute('INSERT INTO product (id, name, caloricity, categories_id) VALUES (?, ?, ?, ?)', (2, 'Греча', 343, 2))
+cursor.execute('INSERT INTO product (id, name, caloricity, categories_id) VALUES (?, ?, ?, ?)', (3, 'Рис', 360, 2))
+cursor.execute('INSERT INTO product (id, name, caloricity, categories_id) VALUES (?, ?, ?, ?)', (4, 'Курица вареная', 170, 3))
+cursor.execute('INSERT INTO product (id, name, caloricity, categories_id) VALUES (?, ?, ?, ?)', (5, 'Яблоко', 52, 4))
+cursor.execute('INSERT INTO product (id, name, caloricity, categories_id) VALUES (?, ?, ?, ?)', (6, 'Банан', 89, 4))
+cursor.execute('INSERT INTO product (id, name, caloricity, categories_id) VALUES (?, ?, ?, ?)', (7, 'Апельсин', 48, 4))
+cursor.execute('INSERT INTO product (id, name, caloricity, categories_id) VALUES (?, ?, ?, ?)', (8, 'Грецкий орех', 654, 5))
+cursor.execute('INSERT INTO product (id, name, caloricity, categories_id) VALUES (?, ?, ?, ?)', (9, 'Кешью', 553, 5))
+cursor.execute('INSERT INTO product (id, name, caloricity, categories_id) VALUES (?, ?, ?, ?)', (10, 'Бородинский хлеб', 259, 6))
+cursor.execute('INSERT INTO product (id, name, caloricity, categories_id) VALUES (?, ?, ?, ?)', (11, 'Хлебцы', 366, 6))
+cursor.execute('INSERT INTO product (id, name, caloricity, categories_id) VALUES (?, ?, ?, ?)', (12, 'Кефир', 40, 7))
+cursor.execute('INSERT INTO product (id, name, caloricity, categories_id) VALUES (?, ?, ?, ?)', (13, 'Творог', 159, 7))
+cursor.execute('INSERT INTO product (id, name, caloricity, categories_id) VALUES (?, ?, ?, ?)', (14, 'Огурец', 15, 8))
+cursor.execute('INSERT INTO product (id, name, caloricity, categories_id) VALUES (?, ?, ?, ?)', (15, 'Помидор', 18, 8))
+
+cursor.execute('INSERT INTO refrigerator_has_product (refrigerator_id, product_id, amount) VALUES (?, ?, ?)', (1, 1, 200))
+cursor.execute('INSERT INTO refrigerator_has_product (refrigerator_id, product_id, amount) VALUES (?, ?, ?)', (1, 2, 300))
+cursor.execute('INSERT INTO refrigerator_has_product (refrigerator_id, product_id, amount) VALUES (?, ?, ?)', (1, 3, 200))
+cursor.execute('INSERT INTO refrigerator_has_product (refrigerator_id, product_id, amount) VALUES (?, ?, ?)', (1, 4, 500))
+cursor.execute('INSERT INTO refrigerator_has_product (refrigerator_id, product_id, amount) VALUES (?, ?, ?)', (1, 5, 200))
+cursor.execute('INSERT INTO refrigerator_has_product (refrigerator_id, product_id, amount) VALUES (?, ?, ?)', (1, 6, 200))
+cursor.execute('INSERT INTO refrigerator_has_product (refrigerator_id, product_id, amount) VALUES (?, ?, ?)', (1, 7, 150))
+cursor.execute('INSERT INTO refrigerator_has_product (refrigerator_id, product_id, amount) VALUES (?, ?, ?)', (1, 8, 40))
+cursor.execute('INSERT INTO refrigerator_has_product (refrigerator_id, product_id, amount) VALUES (?, ?, ?)', (1, 9, 50))
+cursor.execute('INSERT INTO refrigerator_has_product (refrigerator_id, product_id, amount) VALUES (?, ?, ?)', (1, 10, 300))
+cursor.execute('INSERT INTO refrigerator_has_product (refrigerator_id, product_id, amount) VALUES (?, ?, ?)', (1, 11, 150))
+cursor.execute('INSERT INTO refrigerator_has_product (refrigerator_id, product_id, amount) VALUES (?, ?, ?)', (1, 12, 1000))
+cursor.execute('INSERT INTO refrigerator_has_product (refrigerator_id, product_id, amount) VALUES (?, ?, ?)', (1, 13, 500))
+cursor.execute('INSERT INTO refrigerator_has_product (refrigerator_id, product_id, amount) VALUES (?, ?, ?)', (1, 14, 200))
+cursor.execute('INSERT INTO refrigerator_has_product (refrigerator_id, product_id, amount) VALUES (?, ?, ?)', (1, 15, 200))
+
+"""""
+"""""
+1. заполнить таблицы
+2. написать процедуру
+#cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', ('newuser1', 'newuser1@example.com', 28))
+#cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', ('newuser2', 'newuser2@example.com', 29))
+#cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', ('newuser3', 'newuser3@example.com', 22))
+#cursor.execute('SELECT * FROM Users')
+users = cursor.fetchall()
+
+# Выводим результаты
+#for user in users:
+#  print(user)
+cursor.execute('SELECT username FROM Users where age = ?', (28,))
+usernames = cursor.fetchall()
+username = " "
+for user in usernames:
+    print(user)
+
+connection.commit()
+connection.close()
+
+cursor.execute('SELECT min FROM limits')
+limits_min = cursor.fetchall()
+min = numpy.zeros(8)
+k = 0
+for x in limits_min:
+    min[k] = x[0]
+    k = k + 1
+print(min)
+
+cursor.execute('SELECT max FROM limits')
+limits_min = cursor.fetchall()
+max = numpy.zeros(8)
+k = 0
+for x in limits_min:
+    max[k] = x[0]
+    k = k + 1
+print(max)
+connection.commit()
+connection.close()
+
+
+ref_id = 1
+cursor.execute('SELECT refrigerator_id, product_id, caloricity, categories_id FROM refrigerator_has_product JOIN  product WHERE product_id = id AND refrigerator_id = ?', (ref_id,))
+groups = cursor.fetchall()
+
+a = []
+for i in range(1, 9):
+    b = []
+    for x in groups:
+        if(x[3] == i):
+            b = numpy.append(b, x[2])
+    a.append(b)
+for k in a:
+    for i in range(0, len(k)):
+        k[i] = k[i] * 0.001
+
+print(a)
+
+ref_id = 1
+cursor.execute('SELECT refrigerator_id, product_id, amount, categories_id FROM refrigerator_has_product JOIN  product WHERE product_id = id AND refrigerator_id = ?', (ref_id,))
+groups = cursor.fetchall()
+a = []
+for i in range(1, 9):
+    b = []
+    for x in groups:
+        if(x[3] == i):
+            b = numpy.append(b, x[2])
+    a.append(b)
+
+print(a)
+"""""
+connection.commit()
+connection.close()
